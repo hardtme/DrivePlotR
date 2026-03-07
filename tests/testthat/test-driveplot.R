368c35793d115398f1e8aa3497278f73cca357d4
hardtme <hardtme@iastate.edu>
2026-03-07 10:27:51 -0600

    Test calls to driveplot_map()

--- tests/testthat/test-driveplot.R
+++ tests/testthat/test-driveplot.R
@@ -6,3 +6,118 @@ test_that("dataframe throws error", {
     "`shareddata` must be a SharedData object."
   )
 })
+
+# Test calls to driveplot_map() ----
+test_that("if providing lng/lat, must provide both", {
+  drive1 <- nds_data |>
+    dplyr::filter(drive == 1)
+  drive1shared <- crosstalk::SharedData$new(drive1)
+  # lng is provided, but lat is NULL
+  expect_error(
+    driveplot(shareddata = drive1shared,
+              lng = gps_long,
+              lat = NULL,
+              x = time_cst,
+              ys = speed_mph),
+    "If providing `lng` and `lat`, must provide both."
+  )
+  # lat is provided, but not lng
+  expect_error(
+    driveplot(shareddata = drive1shared,
+              lat = gps_lat,
+              x = time_cst,
+              ys = speed_mph),
+    "If providing `lng` and `lat`, must provide both."
+  )
+})
+
+test_that("check spelling of lng/lat", {
+  drive1 <- nds_data |>
+    dplyr::filter(drive == 1)
+  drive1shared <- crosstalk::SharedData$new(drive1)
+  # Misspelled lng
+  expect_error(
+    driveplot(shareddata = drive1shared,
+              lng = gps_lng,
+              lat = gps_lat,
+              x = time_cst,
+              ys = speed_mph),
+    "Can't find `gps_lng` in `shareddata`."
+  )
+  # Misspelled lat
+  expect_error(
+    driveplot(shareddata = drive1shared,
+              lng = gps_long,
+              lat = gps_lt,
+              x = time_cst,
+              ys = speed_mph),
+    "Can't find `gps_lt` in `shareddata`."
+  )
+})
+
+test_that("missing geometry column throws error when lat/lng not provided", {
+  drive1 <- nds_data |>
+    dplyr::filter(drive == 1)
+  drive1shared <- crosstalk::SharedData$new(drive1)
+  expect_error(
+    driveplot(shareddata = drive1shared,
+              x = time_cst,
+              ys = speed_mph),
+    "Can't find a geometry column and `lng` and `lat` not provided."
+  )
+})
+
+test_that("throw error when geometry type isn't POINT", {
+  skip_if_not_installed("sf")
+  drive7_ls <- drive7 |>
+    sf::st_cast("LINESTRING")
+  drive7_ls_sd <- crosstalk::SharedData$new(drive7_ls)
+  expect_error(
+    driveplot(shareddata = drive7_ls_sd,
+              x = time_cst,
+              ys = speed_mph),
+    "Geometry column must have type POINT."
+  )
+})
+
+test_that("works for both ways to specify coordinates", {
+  drive1 <- nds_data |>
+    dplyr::filter(drive == 1)
+  drive1shared <- crosstalk::SharedData$new(drive1)
+  shared_drive <- crosstalk::SharedData$new(drive7)
+  # Specify lat/lng
+  expect_no_error(
+    driveplot(shareddata = drive1shared,
+              lng = gps_long,
+              lat = gps_lat,
+              x = time_cst,
+              ys = speed_mph)
+  )
+
+  # Provide a geometry column with type POINT
+  expect_no_error(
+    driveplot(shareddata = shared_drive,
+            x = time_cst,
+            ys = speed_mph)
+  )
+})
+
+test_that("provide lat/lng but not x and ys", {
+  drive1 <- nds_data |>
+    dplyr::filter(drive == 1)
+  drive1shared <- crosstalk::SharedData$new(drive1)
+  expect_error(
+    driveplot(shareddata = drive1shared,
+              lng = gps_long,
+              lat = gps_lat),
+    "Can't find `<empty>` in `shareddata`."
+  )
+})
+
+test_that("provide geometry column but not x and ys", {
+  shared_drive <- crosstalk::SharedData$new(drive7)
+  expect_error(
+    driveplot(shareddata = shared_drive),
+    "Can't find `<empty>` in `shareddata`."
+  )
+})
