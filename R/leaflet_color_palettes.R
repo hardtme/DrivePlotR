c2e9c05a43d08454e225dcee660125726f83f80b
hardtme <hardtme@iastate.edu>
2026-04-02 13:49:22 -0500

    Add function for leaflet color palettes

---
+++ R/leaflet_color_palettes.R
@@ -0,0 +1,10 @@
+#' Vector of viridis color palettes supported by Leaflet.
+#'
+#' Helper function
+#' The map uses the same color palette as the companion graphs.
+#' @returns Vector of viridis color palettes supported by Leaflet.
+#' @export
+
+leaflet_color_palettes <- function() {
+  c("viridis", "magma", "inferno", "plasma")
+}
